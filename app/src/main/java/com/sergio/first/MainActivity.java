428078e8d268c54a178d7971a34684137fa6931a
SergioTS2101 <sergiots2101@gmail.com>
2021-10-07 08:36:46 +0200

    Segundo commit

---
+++ app/src/main/java/com/sergio/first/MainActivity.java
@@ -0,0 +1,17 @@
+package com.sergio.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author Sergio Tomeo Samuel
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
