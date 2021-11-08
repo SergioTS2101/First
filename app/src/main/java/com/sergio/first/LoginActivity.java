6d84b78134471703e9f89a0667e21580a8755423
SergioTS2101 <sergiots2101@gmail.com>
2021-11-08 12:24:02 +0100

    Optimizando aplicación

--- app/src/main/java/com/sergio/first/LoginActivity.java
+++ app/src/main/java/com/sergio/first/LoginActivity.java
@@ -1,10 +1,14 @@
 package com.sergio.first;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,30 +19,41 @@ import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
  * @author Sergio Tomeo Samuel
  */
 public class LoginActivity extends AppCompatActivity {
-
+    //EditText txt_usuario, txt_contraseña;
+    Button entrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //txt_usuario = (EditText)findViewById(R.id.username);
+        //txt_contraseña = (EditText)findViewById(R.id.txtPassword);
+        entrar = (Button)findViewById(R.id.button);
 
-
-        ImageView mixiGirl =findViewById(R.id.mixi);
+        ImageView mixiPh =findViewById(R.id.mixi);
 
         Glide.with(this)
 
-                .load(R.drawable.mixiiii)
+                .load(R.drawable.mixiiiiii)
                 .transition(DrawableTransitionOptions.withCrossFade(100))
-                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.purple_500)))
-                .into(mixiGirl);
+                //.placeholder(new ColorDrawable(this.getResources().getColor(R.color.purple_500)))
+                .into(mixiPh);
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String usuario = txt_usuario.getText().toString();
+                //String contraseña = txt_contraseña.getText().toString();
+               // if(usuario.equals("sergiots2101") && contraseña.equals("123456")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                //}else{
+                  //  Toast.makeText(getApplicationContext(),"Usuario o contraseña erróneos",Toast.LENGTH_SHORT);
+                //}
+            }
+        });
     }
 
-    // Method for Vaccinate button
-    public void onClick(View v) {
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-    }
 
     public void onClickSign(View v) {
         Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
--- app/src/main/java/com/sergio/first/SignupActivity.java
+++ app/src/main/java/com/sergio/first/SignupActivity.java
@@ -2,6 +2,8 @@ package com.sergio.first;
 
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,18 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class SignupActivity extends AppCompatActivity {
-
+    EditText txt_nombre,txt_usuario, txt_contraseña;
+    Button entrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        txt_nombre=(EditText)findViewById(R.id.txtNombre);
+        txt_usuario=(EditText)findViewById(R.id.editText2);
+        txt_contraseña=(EditText)findViewById(R.id.txtPassword);
+
+        entrar=(Button)findViewById(R.id.button);
 
         ImageView mixiGirl =findViewById(R.id.mixi);
 
