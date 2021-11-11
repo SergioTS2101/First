4589913e2ac8abdf399e8305dfa7a113440fa6c6
SergioTS2101 <sergiots2101@gmail.com>
2021-11-11 09:25:42 +0100

    Recuperando proyecto

--- app/src/main/java/com/sergio/first/LoginActivity.java
+++ app/src/main/java/com/sergio/first/LoginActivity.java
@@ -1,16 +1,12 @@
 package com.sergio.first;
 
-import android.app.DownloadManager;
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
